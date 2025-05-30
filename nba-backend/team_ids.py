396d15ba00b1e252abb95381c015b9a7a7a8a56a
nitin <85755628+nitinvunnam@users.noreply.github.com>
2025-05-30 13:57:49 -0500

    Update team_ids.py

--- nba-backend/team_ids.py
+++ nba-backend/team_ids.py
@@ -1,11 +1,8 @@
 from nba_api.stats.static import teams
 
-# Retrieve the list of NBA teams
 nba_teams = teams.get_teams()
 
-# Create a dictionary mapping team IDs to team full names
 team_id_map = {team['id']: team['full_name'] for team in nba_teams}
 
-# Display the team IDs and their corresponding names
 for team_id, name in team_id_map.items():
     print(f'{team_id}, "{name}"')
