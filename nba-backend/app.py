cb4604b935824626b24220aebe9add592c7298a2
nitin <85755628+nitinvunnam@users.noreply.github.com>
2025-06-16 14:56:10 -0500

    changed port to render specific one

--- nba-backend/app.py
+++ nba-backend/app.py
@@ -32,8 +32,7 @@ current_year = datetime.datetime.now().year
 def get_filtered_player_names():
     with open("valid_initial_players.json", "r") as f:
         return json.load(f)
-
-
+    
 @app.route("/players", methods=["GET"])
 def get_players(): 
     return jsonify(list(cached_season_data.keys()))  # Full dropdown pool
@@ -43,7 +42,7 @@ def initial_players():
     today = date.today().isoformat()
     data = load_daily_players()
     if data["date"] != today:
-        random_players = random.sample(get_filtered_player_names, 2)
+        random_players = random.sample(get_filtered_player_names(), 2)
         save_daily_players(today, random_players)
         return jsonify(random_players)  # Filtered pool
     else:
@@ -57,5 +56,5 @@ def player_stats():
     return jsonify(cached_season_data[name])
 
 if __name__ == "__main__":
-    app.run(port=5001, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  # use Render-assigned port
+    app.run(host="0.0.0.0", port=port)
